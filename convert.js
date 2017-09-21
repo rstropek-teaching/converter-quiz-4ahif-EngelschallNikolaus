435c589d7128c2d43d1ebe42798d2a0e8cf90600
EngelschallNikolaus <31962790+EngelschallNikolaus@users.noreply.github.com>
2017-09-21 13:18:12 +0200

    Add files via upload

--- convert.js
+++ convert.js
@@ -1,1 +1,4 @@
-console.log('Not implemented yet!');
+if(require('convert-units')().possibilities().indexOf(process.argv[3]) !== -1 && require('convert-units')().from(process.argv[3]).possibilities().indexOf(process.argv[5])!==-1 && parseInt(process.argv[2]) && process.argv[4]==="to" && process.argv.length===6)
+    console.log(process.argv[2]+" "+process.argv[3]+" are "+require('convert-units')(process.argv[2]).from(process.argv[3]).to(process.argv[5])+" "+process.argv[5])
+else
+    console.log('Invalid parameters');
